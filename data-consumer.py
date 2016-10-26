16d77baaac4484e0a9e5dd4b15987494476ddc8c
yangcareers <yangcareers@gmail.com>
2016-10-26 15:40:13 -0400

    commit

---
+++ data-consumer.py
@@ -0,0 +1,6 @@
+# - consumer data from kafka
+
+from kafka import KafkaConsumer
+consumer = KafkaConsumer('stock-analyzer', bootstrap_servers='192.168.99.100:9092')
+for msg in consumer:
+    print(msg)
